d328407f64ce770566e3746bdbf5a543dcea1824
Harry Wolff <hswolff@gmail.com>
2015-05-30 23:05:08 -0400

    Don't have header link to anywhere

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -9,9 +9,9 @@ module.exports = React.createClass({
     return (
       <header className="row header">
         <div className="col-md-12">
-          <a href="http://blog.hswolff.com/" className="header-link">
+          <div className="header-link">
             <h1 className="header-text">Harry <div className="header-link-img" /> Wolff</h1>
-          </a>
+          </div>
         </div>
       </header>
     );
